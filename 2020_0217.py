e7bcfd666ab32216919434db59698ceb6884e12c
akihanari <akihanari@gmail.com>
2020-02-18 13:07:18 +0900

    2020_0218_1307

--- 2020_0217.py
+++ 2020_0217.py
@@ -14,7 +14,7 @@ print(sample_list2)
 # **{式 : 式 for 変数 in リスト if 条件}**<br>
 
 # In[]:
-sample_dict1 = {'a':100, 'b':120, 'c':200, 'd':300, 'e':400}
+sample_dict1 = {'a': 100, 'b': 120, 'c': 200, 'd': 300, 'e': 400}
 print(sample_dict1)
 sample_dict2 = {i*3 for i in sample_dict1 if i >= 'c'}
 print(sample_dict2)
@@ -38,32 +38,32 @@ print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = () #中身が空
+sample = ()  # 中身が空
 print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = ('a') #要素が1つだけの場合は後ろに','をつけないとタプルにならない
+sample = ('a')  # 要素が1つだけの場合は後ろに','をつけないとタプルにならない
 print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = ('a',) #要素が1つだけの場合は後ろに','をつける
+sample = ('a',)  # 要素が1つだけの場合は後ろに','をつける
 print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = 'a', #()を省略
+sample = 'a',  # ()を省略
 print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = list(['a', 'b', 'c']) #タプル⇒リスト
+sample = list(['a', 'b', 'c'])  # タプル⇒リスト
 print("sample:", sample)
 print("type:", type(sample))
 
 # In[]:
-sample = tuple(['a', 'b', 'c']) #リスト⇒タプル
+sample = tuple(['a', 'b', 'c'])  # リスト⇒タプル
 print("sample:", sample)
 print("type:", type(sample))
 
@@ -71,7 +71,7 @@ print("type:", type(sample))
 sample = ('a', 'b', 'c', 'd', 'e')
 print("sample:", sample)
 print("type:", type(sample))
-print("sample[0]:", sample[0]) #インデックスを指定して要素を取り出す
+print("sample[0]:", sample[0])  # インデックスを指定して要素を取り出す
 print("type:", type(sample[0]))
 
 # In[]:
@@ -84,7 +84,7 @@ for a in enumerate(sample):
 # In[]:
 sample = ('a', 'b', 'c', 'd', 'e')
 print(sample)
-print(sample[2:5:2]) #タプル名[開始:終了:ステップ(間隔)]
+print(sample[2:5:2])  # タプル名[開始:終了:ステップ(間隔)]
 
 
 # In[]:
