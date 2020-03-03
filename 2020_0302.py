ae3d42493db4ec2e24dd3ed6f0b889eaca8b4e50
akihanari <akihanari@gmail.com>
2020-03-03 15:35:45 +0900

    2020_0303_1535

--- 2020_0302.py
+++ 2020_0302.py
@@ -27,6 +27,9 @@ print("カウント:", SampleClass.getCount())
 p2 = SampleClass("Tom", 220)  # インスタンスの作成
 print("名前は", p2.getName(), ", 所持金は", p2.getMoney(), "です。")
 print("カウント:", SampleClass.getCount())
+
+print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+print("カウント:", SampleClass.getCount())
 # In[]:
 
 
@@ -88,62 +91,122 @@ p1.money = -100  # インスタンス変数を書き換える
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 
 # In[]:
+# クラス変数をプライベートにする
 
 
 class SampleClass:
-    __count = 0  # クラス変数
+    __count = 0  # プライベート変数
 
-    def __init__(self, name, money):  # コンストラクタ
+    def __init__(self, name, money):
         SampleClass.__count += 1
         self.name = name
-        self.__money = money
+        self.money = money
 
-    def getName(self):  # インスタンスメソッド
+    def getName(self):
         return self.name
 
-    def getMoney(self):  # インスタンスメソッド
-        return self.__money
+    def getMoney(self):
+        return self.money
 
     @classmethod
-    def getCount(cls):  # クラスメソッド
+    def getCount(cls):
         return cls.__count
 
 
 p1 = SampleClass("Alice", 200)  # インスタンスの作成
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 print("カウント:", SampleClass.__count)
-SampleClass.__count = 10  # クラス変数を書き換える
+SampleClass.__count = 10  # クラス変数を書き換えられない
 print("カウント:", SampleClass.__count)
-p1.money = -100  # インスタンス変数を書き換える
+
+# In[]:
+# インスタンス変数をプライベートにする
+
+
+class SampleClass:
+    count = 0
+
+    def __init__(self, name, money):
+        SampleClass.count += 1
+        self.name = name
+        self.__money = money  # プライベート変数
+
+    def getName(self):
+        return self.name
+
+    def getMoney(self):
+        return self.__money
+
+    @classmethod
+    def getCount(cls):
+        return cls.count
+
+
+p1 = SampleClass("Alice", 200)  # インスタンスの作成
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+print(p1.name)
+print(p1.__money)  # アクセス不可
 
 # In[]:
 
 
 class SampleClass:
-    __count = 0  # クラス変数
+    __count = 0
 
-    def __init__(self, name, money):  # コンストラクタ
+    def __init__(self, name, money):
         SampleClass.__count += 1
+        self.name = name
+        self.money = money
+
+    def getName(self):
+        return self.name
+
+    def getMoney(self):
+        return self.money
+
+    def setMoney(self, money2):
+        self.money = money2
+
+    @classmethod
+    def getCount(cls):
+        return cls.__count
+
+
+p1 = SampleClass("Alice", 200)
+print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+p1.setMoney(300)
+print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+print("カウント:", p1.getCount())
+
+# In[]:
+
+
+class SampleClass:
+    count = 0
+
+    def __init__(self, name, money):
+        SampleClass.count += 1
         self.name = name
         self.__money = money
 
-    def getName(self):  # インスタンスメソッド
+    def getName(self):
         return self.name
 
-    def getMoney(self):  # インスタンスメソッド
+    def getMoney(self):
         return self.__money
 
+    def setMoney(self, money2):
+        self.__money = money2
+
     @classmethod
-    def getCount(cls):  # クラスメソッド
-        return cls.__count
+    def getCount(cls):
+        return cls.count
 
 
 p1 = SampleClass("Alice", 200)  # インスタンスの作成
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
-print("カウント:", SampleClass.getCount())
-p1._SampleClass__count = 10  # クラス変数を書き換える
-print("カウント:", SampleClass.__count)
-# p1.money = -100  # インスタンス変数を書き換える
-# print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+p1.setMoney(300)
+print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+
+
 # In[]:
