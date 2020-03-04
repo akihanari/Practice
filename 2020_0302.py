e03b35b9d53bbb53452148e30344c045d0224932
akihanari <akihanari@gmail.com>
2020-03-04 15:24:27 +0900

    2020_0304

--- 2020_0302.py
+++ 2020_0302.py
@@ -41,24 +41,12 @@ class SampleClass:
         self.name = name
         self.money = money
 
-    def getName(self):  # インスタンスメソッド
-        return self.name
-
-    def getMoney(self):  # インスタンスメソッド
-        return self.money
-
-    @classmethod
-    def getCount(cls):  # クラスメソッド
-        return cls.count
-
 
 p1 = SampleClass("Alice", 200)  # インスタンスの作成
-print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+# 外部からインスタンス変数にアクセス
+print("名前は", p1.name, ", 所持金は", p1.money, "です。")
+# 外部からクラス変数にアクセス
 print("カウント:", SampleClass.count)
-SampleClass.count = 10  # クラス変数を書き換える
-print("カウント:", SampleClass.count)
-p1.money = -100  # インスタンス変数を書き換える
-print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 
 # In[]:
 
@@ -71,31 +59,56 @@ class SampleClass:
         self.name = name
         self._money = money
 
-    def getName(self):  # インスタンスメソッド
-        return self.name
-
-    def getMoney(self):  # インスタンスメソッド
-        return self._money
-
-    @classmethod
-    def getCount(cls):  # クラスメソッド
-        return cls._count
-
 
 p1 = SampleClass("Alice", 200)  # インスタンスの作成
-print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
+# 外部からインスタンス変数にアクセス
+print("名前は", p1.name, ", 所持金は", p1._money, "です。")
+# 外部からクラス変数にアクセス
 print("カウント:", SampleClass._count)
-SampleClass._count = 10  # クラス変数を書き換える
-print("カウント:", SampleClass._count)
-p1.money = -100  # インスタンス変数を書き換える
-print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 
 # In[]:
 # クラス変数をプライベートにする
 
 
 class SampleClass:
-    __count = 0  # プライベート変数
+    __count = 0  # クラス変数
+
+    def __init__(self, name, money):  # コンストラクタ
+        SampleClass.__count += 1
+        self.name = name
+        self.money = money
+
+
+p1 = SampleClass("Alice", 200)  # インスタンスの作成
+# 外部からインスタンス変数にアクセス
+print("名前は", p1.name, ", 所持金は", p1.money, "です。")
+# 外部からクラス変数にアクセス
+print("カウント:", SampleClass.__count)
+
+# In[]:
+# インスタンス変数をプライベートにする
+
+
+class SampleClass:
+    count = 0  # クラス変数
+
+    def __init__(self, name, money):  # コンストラクタ
+        SampleClass.count += 1
+        self.name = name
+        self.__money = money
+
+
+p1 = SampleClass("Alice", 200)  # インスタンスの作成
+# 外部からインスタンス変数にアクセス
+print("名前は", p1.name, ", 所持金は", p1.__money, "です。")
+# 外部からクラス変数にアクセス
+print("カウント:", SampleClass.count)
+
+# In[]:
+
+
+class SampleClass:
+    __count = 0
 
     def __init__(self, name, money):
         SampleClass.__count += 1
@@ -108,44 +121,27 @@ class SampleClass:
     def getMoney(self):
         return self.money
 
+    def setMoney(self, money2):
+        self.money = money2
+
     @classmethod
     def getCount(cls):
         return cls.__count
 
-
-p1 = SampleClass("Alice", 200)  # インスタンスの作成
-print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
-print("カウント:", SampleClass.__count)
-SampleClass.__count = 10  # クラス変数を書き換えられない
-print("カウント:", SampleClass.__count)
-
-# In[]:
-# インスタンス変数をプライベートにする
-
-
-class SampleClass:
-    count = 0
-
-    def __init__(self, name, money):
-        SampleClass.count += 1
-        self.name = name
-        self.__money = money  # プライベート変数
-
-    def getName(self):
-        return self.name
-
-    def getMoney(self):
-        return self.__money
-
     @classmethod
-    def getCount(cls):
-        return cls.count
+    def setCount(cls, count2):
+        cls.__count = count2
 
 
-p1 = SampleClass("Alice", 200)  # インスタンスの作成
+p1 = SampleClass("Alice", 200)
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
-print(p1.name)
-print(p1.__money)  # アクセス不可
+print("カウント:", p1.getCount())
+p1.setCount(100)
+print("カウント:", p1.getCount())
+p2 = SampleClass("Tom", 300)
+print("名前は", p2.getName(), ", 所持金は", p2.getMoney(), "です。")
+print("カウント:", p1.getCount())
+print("カウント:", p2.getCount())
 
 # In[]:
 
@@ -177,7 +173,6 @@ print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 p1.setMoney(300)
 print("名前は", p1.getName(), ", 所持金は", p1.getMoney(), "です。")
 print("カウント:", p1.getCount())
-
 # In[]:
 
 
